41587c44ed7fbbc5ac31d3a1dd515d53be7f30f4
WojciechSala <wojteksala190@gmail.com>
2020-08-17 20:40:37 +0200

    Hamburger

---
+++ src/components/Hamburger.js
@@ -0,0 +1,10 @@
+import React from "react"
+import styled from "styled-components"
+
+const Hamburger = () => {
+  return <Container></Container>
+}
+
+export default Hamburger
+
+const Container = styled.div``
