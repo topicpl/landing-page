c4c743c427e685fa453535103c84b94cd3a44919
WojciechSala <wojteksala190@gmail.com>
2020-08-28 01:43:52 +0200

    404

--- src/pages/404/index.js
+++ src/pages/404/index.js
@@ -1,10 +1,42 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import styled from "styled-components"
+import detectBrowserLanguage from "detect-browser-language"
+import MasterStyle from "../../assets/styles/MasterStyle.js"
+import Navbar from "../../components/Navbar"
 
-const page404 = () => {
-  return <Container>404</Container>
+const Page404 = () => {
+  const [lang, setLang] = useState("pl")
+
+  useEffect(() => {
+    const detectedLang = detectBrowserLanguage()
+    if (detectedLang.includes("pl")) setLang("pl")
+    else setLang("en")
+  }, [lang])
+
+  return (
+    <MasterStyle>
+      <Navbar lang={lang} />
+      <Container>
+        <Top>404</Top>
+        <Bottom>ERROR</Bottom>
+      </Container>
+    </MasterStyle>
+  )
 }
 
-export default page404
+export default Page404
 
-const Container = styled.div``
+const Container = styled.div`
+  font-weight: ${({ theme }) => theme.font.weight.black};
+  height: calc(100vh - 200px);
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`
+const Top = styled.div`
+  font-size: 10vw;
+`
+const Bottom = styled.div`
+  font-size: 5vw;
+`
